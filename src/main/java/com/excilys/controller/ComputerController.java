945de65c39de4db14a13fbc24168120f063a9c32
babstou92 <bastien.perrin.hds@gmail.com>
2020-01-30 12:04:09 +0100

    test

--- src/main/java/com/excilys/controller/ComputerController.java
+++ src/main/java/com/excilys/controller/ComputerController.java
@@ -1,14 +1,12 @@
 package com.excilys.controller;
 
 import java.util.List;
-
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
 import com.excilys.dto.ComputerDTO;
 import com.excilys.exception.CheckDateIntervale;
 import com.excilys.exception.CheckNameException;
